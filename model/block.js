6f7696c8f0cb71fee9f80f204fb37a1c2bc903b5
Gonzalo Rodriguez <gonzalo@rodriguez-blanco.com>
2018-10-20 18:56:23 +0200

    style: indentation

--- model/block.js
+++ model/block.js
@@ -1,9 +1,9 @@
 module.exports = class Block {
     constructor(data) {
         this.hash = "",
-            this.height = 0,
-            this.body = data,
-            this.time = 0,
-            this.previousBlockHash = ""
+        this.height = 0,
+        this.body = data,
+        this.time = 0,
+        this.previousBlockHash = ""
     }
 }
